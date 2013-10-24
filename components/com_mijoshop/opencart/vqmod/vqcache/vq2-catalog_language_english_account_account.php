6079a7efe6826eb9ca90a5fbdbb131d2143f322f
trung nguyen thanh <trungnt@exoplatform.com>
2013-10-24 22:54:02 +0700

    update login button

---
+++ components/com_mijoshop/opencart/vqmod/vqcache/vq2-catalog_language_english_account_account.php
@@ -0,0 +1,31 @@
+<?php
+/*
+* @package		MijoShop
+* @copyright	2009-2013 Mijosoft LLC, mijosoft.com
+* @license		GNU/GPL http://www.gnu.org/copyleft/gpl.html
+* @license		GNU/GPL based on AceShop www.joomace.net
+*/
+
+// No Permission
+defined('_JEXEC') or die('Restricted access');
+
+// Heading 
+$_['heading_title']      = 'My Account';
+
+// Text
+$_['text_account']       = 'Account';
+$_['text_my_account']    = 'My Account';
+$_['text_my_orders']     = 'My Orders';
+$_['text_my_newsletter'] = 'Newsletter';
+$_['text_edit']          = 'Edit your account information';
+$_['text_password']      = 'Change your password';
+$_['text_address']       = 'Modify your address book entries';
+$_['text_wishlist']      = 'Modify your wish list';
+$_['text_order']         = 'View your order history';
+$_['text_download']      = 'Downloads';
+$_['text_reward']        = 'Your Reward Points'; 
+$_['text_return']        = 'View your return requests'; 
+$_['text_transaction']   = 'Your Transactions'; 
+$_['text_newsletter']    = 'Subscribe / unsubscribe to newsletter';
+$_['text_logout']    = 'Logout';
+?>
---
+++ logs/error.php
@@ -0,0 +1,7 @@
+#
+#<?php die('Forbidden.'); ?>
+#Date: 2013-10-24 15:06:24 UTC
+#Software: Joomla Platform 12.2.0 Stable [ Neil Armstrong ] 21-September-2012 00:00 GMT
+
+#Fields: datetime	priority	category	message
+2013-10-24T15:06:24+00:00	INFO	joomlacanceled	
